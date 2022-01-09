64f134b5b4dd908a11d1843b95917318c533f75b
Omer Kacar <omerkacar1@gmail.com>
2022-01-09 15:09:04 +0000

    feat(core): add upload PDF file

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ app.use('/js', express.static('node_modules/jquery/dist')); //jquery
 let todoArray = [];
 
 //Set up application to handle GET requests sent to the task path
-app.get('/todo/*', handleGetRequest);//Returns task with specified ID
-app.get('/todo', handleGetRequest);//Returns all tasks
+// app.get('/todo/*', handleGetRequest);//Returns task with specified ID
+// app.get('/todo', handleGetRequest);//Returns all tasks
 
 //set up application to handle DELETE requests sent to the task path
-app.delete('/todo', handleDeleteRequest);
-
+// app.delete('/todo', handleDeleteRequest);
+//
 //Set up application to handle POST requests sent to the task path
-app.post('/todo', handlePostRequest);//Adds a newtask 
+// app.post('/todo', handlePostRequest);//Adds a newtask
 
 //Start the app listening on port 8080
 app.listen(8080);
@@ -40,126 +40,14 @@ const connectionPool = mysql.createPool({
     debug: false
 });
 
-//Handles DELETE requests to our web service
-function handleDeleteRequest(request, response) {
+const fs = require('fs'),
+    PDFParser = require("pdf2json");
 
-    //Output the data sent to the server
-    let newTask = request.body
-    console.log("Data received: " + JSON.stringify(newTask));
+const pdfParser = new PDFParser(this,1);
 
-    //Finish off the interaction.
-    response.send("Task removed successfully.");
+pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
+pdfParser.on("pdfParser_dataReady", pdfData => {
+    fs.writeFile("text.txt", pdfParser.getRawTextContent(), ()=>{console.log("Done.");});
+});
 
-    //Build SQL query
-    let sql = 'DELETE FROM tasks WHERE id="' + request.body.id + '"';
-
-    //Execute query and output results
-    connectionPool.query(sql, (err, result) => {
-        if (err) {//Check for errors
-            console.error("Error executing query: " + JSON.stringify(err));
-        }
-        else {
-            console.log(JSON.stringify(result));
-        }
-    });
-
-}
-
-//Handles GET requests to our web service
-function handleGetRequest(request, response) {
-    let todoArray = [];
-    /* Outputs all of the todos */
-    //Build query
-    let sql = "SELECT * FROM tasks";
-
-    //Split the path of the request into its components
-    var pathArray = request.url.split("/");
-
-    //Get the last part of the path
-    var pathEnd = pathArray[pathArray.length - 1];
-    //Execute query and output results
-    connectionPool.query(sql, (err, result) => {
-        if (err) {//Check for errors
-            console.error("Error executing query: " + JSON.stringify(err));
-        }
-        else {//Output results in JSON format - a web service would return this string.
-
-            //If path ends with 'todo' we return all tasks
-            if (pathEnd === 'todo') {
-                response.send(JSON.stringify(result));
-            }
-            else
-            {
-            //send back data from mysql
-            response.send(JSON.stringify(result));
-            todoArray.push(JSON.stringify(result));
-            }
-        }
-    });
-
-
-}
-
-//Handles GET requests to our web service for testing
-function handleGetRequest(request, response, task) {
-    let todoArray = [];
-    /* Outputs all of the todos */
-    //Build query
-    let sql = "SELECT * FROM tasks";
-
-    //Split the path of the request into its components
-    var pathArray = request.url.split("/");
-
-    //Get the last part of the path
-    var pathEnd = pathArray[pathArray.length - 1];
-    //Execute query and output results
-    connectionPool.query(sql, (err, result) => {
-        if (err) {//Check for errors
-            console.error("Error executing query: " + JSON.stringify(err));
-        }
-        else {//Output results in JSON format - a web service would return this string.
-
-            //If path ends with 'todo' we return all tasks
-            if (pathEnd === 'todo') {
-                response.send(JSON.stringify(result));
-            }
-            else
-            {
-            //send back data from mysql
-            response.send(JSON.stringify(result));
-            todoArray.push(JSON.stringify(result));
-            }
-        }
-    });
-
-
-}
-
-//Handles POST requests to our web service
-function handlePostRequest(request, response) {
-
-    //Output the data sent to the server
-    let newTask = request.body
-    console.log("Data received: " + JSON.stringify(newTask));
-
-    //Finish off the interaction.
-    response.send("Task added successfully.");
-
-    //Build query
-    let sql = "INSERT INTO tasks (task, dateAdded) "
-        + " VALUES ("//unique id for task
-        + "\"" + request.body.task //task details
-        + "\", \"" + new Date().toLocaleString('en-US', { timeZone: 'GB-Eire' }) + "\"" //date
-        + ")";
-
-    //Execute query and output results
-    connectionPool.query(sql, (err, result) => {
-        if (err) {//Check for errors
-            console.error("Error executing query: " + JSON.stringify(err));
-        }
-        else {
-            console.log(JSON.stringify(result));
-        }
-    });
-
-}
+pdfParser.loadPDF("C:\\Users\\omerk\\University\\Individual-Undergraduate-Project\\public\\sample.pdf");
