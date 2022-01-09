31d09ed334a85e17c982bbf28a5e8a89ee4e4307
Omer Kacar <omerkacar1@gmail.com>
2022-01-09 22:24:12 +0000

    feat(core): add user login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const url = require('url');
 
 //Create express app and configure it with body-parser
 const app = express();
@@ -20,7 +21,8 @@ let todoArray = [];
 // app.get('/todo/*', handleGetRequest);//Returns task with specified ID
 // app.get('/todo', handleGetRequest);//Returns all tasks
 // to get registration form
-app.post('/register/*', registerUser);
+app.get('/signin', signInUser);
+app.post('/signup', signUpUser);
 //set up application to handle DELETE requests sent to the task path
 // app.delete('/todo', handleDeleteRequest);
 //
@@ -41,8 +43,34 @@ const connectionPool = mysql.createPool({
     debug: false
 });
 
+//Handles GET requests to the web service
+function signInUser(request, response) {
+    //Parse the URL
+    var urlObj = url.parse(request.url, true);
+
+    //Extract object containing queries from URL object.
+    var queries = urlObj.query;
+
+    //Get the pagination properties if they have been set. Will be undefined if not set.
+    var username = queries['username'];
+    var password = queries['password'];
+    username= username.replace("%20", '');
+    console.log(username + " " + password);
+    let sql = 'SELECT * FROM user_details WHERE username=' + username + ' AND PASSWORD=' + password;
+console.log(sql);
+    // Execute query and output results
+    connectionPool.query(sql, (err, result) => {
+        if (err) {//Check for errors
+            console.error("Error executing query: " + JSON.stringify(err));
+        } else {
+            response.send(JSON.stringify(result));
+            console.log(JSON.stringify(result));
+        }
+    });
+}
+
 //Handles POST requests to our web service
-function registerUser(request, response) {
+function signUpUser(request, response) {
 
     //Output the data sent to the server
     let details = request.body
@@ -51,20 +79,17 @@ function registerUser(request, response) {
     //Finish off the interaction.
     response.send("User added successfully.");
 
-    console.log("username: " + details.username + " ... password: " + details.password);
     //Build query
     let sql = "INSERT INTO user_details (username, password) "
         + " VALUES ("//unique id for task
         + "\"" + details.username //task details
         + "\", \"" + details.password + "\"" //date
         + ");";
-    console.log(sql);
     // Execute query and output results
     connectionPool.query(sql, (err, result) => {
         if (err) {//Check for errors
             console.error("Error executing query: " + JSON.stringify(err));
-        }
-        else {
+        } else {
             console.log(JSON.stringify(result));
         }
     });
