a91a4d069455d31938389053c5aa1ff3f7c3a5e6
Omer Kacar <ok243@live.mdx.ac.uk>
2022-03-04 16:28:42 +0000

    build(core): add MVC template

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const express = require('express')
 // Create Express app
 const app = express()
 
+app.set('view engine', 'ejs');
+//Routes
+app.use('/', require('./routes/login'));
 // A sample route
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
 // Start the Express server
-app.listen(3000, () => console.log('Server running on port 3000!'))
+const PORT = 8080;
+app.listen(PORT, () => console.log('Server running on port' + PORT));
