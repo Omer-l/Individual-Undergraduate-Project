cf977c106c1dbc99f3661f2301a2ec1b64b584cf
Omer Kacar <ok243@live.mdx.ac.uk>
2022-03-19 10:30:49 +0000

    refactor(core): move uploadFile() to client.js

--- public/client.js
+++ public/client.js
@@ -147,3 +147,34 @@ function userLoggedIn() {
     xhttp.open("GET", "/checklogin", true);
     xhttp.send();
 }
+//Uploads file to server side
+function uploadFile() {
+//        Reference to the div element for server responses
+    let serverResponse = $('#ServerResponse');
+    serverResponse.text("");
+
+//    Gets the file to send and upload to server-side
+    let fileArray = document.getElementById("FileInput").files;
+//    Ensures file is selected
+    if(fileArray.length !== 1) {
+        serverResponse.text("Please select file to upload.");
+        return;
+    }
+//    Wrap file inside FormData object
+    const formData = new FormData();
+    formData.append('myFile', fileArray[0]);
+//    Sets up HTTP req to send file and receive message of confirmation
+    let httpReq = new XMLHttpRequest();
+    httpReq.onload = () => {
+        console.log(httpReq.responseText);
+        let response = JSON.parse(httpReq.responseText);
+        if("error" in response) //pdf could not be uploaded
+            serverResponse.text(response.error);
+        else //success
+            serverResponse.text("File uploaded successfully");
+    };
+
+//    Sends off message to upload file
+    httpReq.open("POST", '/upload');
+    httpReq.send(formData);
+}
