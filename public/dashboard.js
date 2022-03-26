9a77f09f68c6ddee35df02a414231aa839e467e0
Omer Kacar <ok243@live.mdx.ac.uk>
2022-03-26 08:16:20 +0000

    feat(core): add highlighting of words already read

--- public/dashboard.js
+++ public/dashboard.js
@@ -10,6 +10,7 @@ function outputDashboard() {
         '<button class="btn btn-lg btn-dark"  onclick="uploadFile()">Upload PDF</button>' +
         '<div id="ServerResponse"></div>' +
         '<span id="w1" onmouseover=highlight(this) onmouseout="unhighlight(this)">Testing</span> <span id="w2" onmouseover=highlight(this) onmouseout="unhighlight(this)">Hover</span> <span id="w3" onmouseover=highlight(this) onmouseout="unhighlight(this)">Ipsum</span> <span id="w4" onmouseover=highlight(this) onmouseout="unhighlight(this)">Sit</span> <span id="w5" onmouseover=highlight(this) onmouseout="unhighlight(this)">Ame</span>' +
+        '<span id="w6" onmouseover=highlight(this) onmouseout="unhighlight(this)">Testing</span> <span id="w7" onmouseover=highlight(this) onmouseout="unhighlight(this)">Hover</span> <span id="w8" onmouseover=highlight(this) onmouseout="unhighlight(this)">Ipsum</span> <span id="w9" onmouseover=highlight(this) onmouseout="unhighlight(this)">Sit</span> <span id="w10" onmouseover=highlight(this) onmouseout="unhighlight(this)">Ame</span>' +
         '<h1>Your PDFs</h1>' +
         '<div id="UserPdfsList"></div>' +
         '<div id="holder"></div>';
--- public/pdfviewer.js
+++ public/pdfviewer.js
@@ -1,7 +1,10 @@
-let highlightColor = "yellow";
-let unhighlightColor = "lightblue";
-let fieldOfView = 1;
-let wordPrefix = "w";
+let highlightColor = "yellow"; //Word currently being read
+let unhighlightColor = "lightblue"; //already read words
+let backgroundColor = "white";
+let fieldOfView = 1; //how much the user can read in field of view
+let wordIdPrefix = "w"; //Prefix of word's DOM element ID
+let previouslyReadWordIndex = 0; //For highlighting the words correctly, to prevent accidental jumps in reading
+let fieldOfViewError = fieldOfView + 1; //Room for error in reading accident jumps
 
 /** Gets word number from an id i.e., w4 would return 4*/
 function getWordNumber(word) {
@@ -14,8 +17,9 @@ function highlight(word) {
     let idOfWordBeingLookedAt = getWordNumber(word);
     let startOfFieldOfView = idOfWordBeingLookedAt - fieldOfView;
     let endOfFieldOfView = idOfWordBeingLookedAt + fieldOfView;
+    //Highlights actively read
     for(let highlightIndex = startOfFieldOfView; highlightIndex <= endOfFieldOfView; highlightIndex++) {
-        let wordId = wordPrefix + highlightIndex;
+        let wordId = wordIdPrefix + highlightIndex;
         let wordInFieldOfView = document.getElementById(wordId);
         wordInFieldOfView.style.backgroundColor = highlightColor;
     }
@@ -26,9 +30,26 @@ function unhighlight(word) {
     let idOfWordBeingLookedAt = getWordNumber(word);
     let startOfFieldOfView = idOfWordBeingLookedAt - fieldOfView;
     let endOfFieldOfView = idOfWordBeingLookedAt + fieldOfView;
-    for(let highlightIndex = startOfFieldOfView; highlightIndex <= endOfFieldOfView; highlightIndex++) {
-        let wordId = wordPrefix + highlightIndex;
+    //unhighlights words not currently being read
+    for(let unhighlightIndex = startOfFieldOfView; unhighlightIndex <= endOfFieldOfView; unhighlightIndex++) {
+        let wordId = wordIdPrefix + unhighlightIndex;
         let wordInFieldOfView = document.getElementById(wordId);
-        wordInFieldOfView.style.backgroundColor = unhighlightColor;
+        wordInFieldOfView.style.backgroundColor = backgroundColor;
     }
+    //Unhighlights all previous words
+    let readingAtCorrectPace = idOfWordBeingLookedAt <= previouslyReadWordIndex + fieldOfViewError;
+    console.log(idOfWordBeingLookedAt + " COMP TO: " + (previouslyReadWordIndex + fieldOfViewError));
+    if(readingAtCorrectPace) { //ensures reader is not jumping text
+        previouslyReadWordIndex = idOfWordBeingLookedAt;
+        console.log(readingAtCorrectPace);
+        //highlights words not currently being read
+        for(let highlightIndex = previouslyReadWordIndex - 1; highlightIndex <= endOfFieldOfView; highlightIndex++) {
+            let wordId = wordIdPrefix + highlightIndex;
+            let wordInFieldOfView = document.getElementById(wordId);
+            let highlighted = wordInFieldOfView.style == unhighlightColor;
+            if(!highlighted)
+                wordInFieldOfView.style.backgroundColor = unhighlightColor;
+        }
+    }
+
 }
